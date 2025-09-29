f167eafdb92c67bcc44c802bd164f11d703f2c3d
Hemanagaraju21 <naga.sntv@gmail.com>
2025-09-29 15:33:55 +0530

    Sharing of eclipse project to Git

---
+++ src/main/java/oops_concepts/Basic_class.java
@@ -0,0 +1,11 @@
+package oops_concepts;
+
+public class Basic_class {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+        System.out.println("first class creation");
+	}
+
+}
